a6d19cbea1b41f1f025b6d0ad05b7aa6a7a7935b
Leslye Pérez <leslye@klu.mx>
2021-10-12 18:46:26 -0500

    para descargar cambios

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'db_peiboappv1' );
+define( 'DB_NAME', 'peibojorge' );
 #define( 'DB_NAME', 'peibo' );
 
 /** MySQL database username */
