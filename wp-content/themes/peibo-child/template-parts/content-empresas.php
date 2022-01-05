83d629a425c45394ae684f012f4337bf49bed8da
Jorge Malagon <jorge@kiper.app>
2022-01-05 14:37:03 -0600

    Ajuste texto home

--- wp-content/themes/peibo-child/template-parts/content-empresas.php
+++ wp-content/themes/peibo-child/template-parts/content-empresas.php
@@ -28,7 +28,7 @@ get_header();
             <label class="animate__animated animate__bounce animate__delay-1s animate__fadeInLeftBig">Realiza dispersiones masivas de pagos a toda tu nómina, proveedores o vendedores y agiliza tu proceso de pago.</label>
           </div>
           <div class="col-md-12 col-xs-10 offset-xs-1 offset-md-0 d-flex justify-content-center noMargin">
-            <p class="animate__animated animate__bounce animate__delay-1s animate__fadeInLeftBig"><small>* Aplícan Términos y Condiciones.</small></p>
+            <p class="animate__animated animate__bounce animate__delay-1s animate__fadeInLeftBig"><small>* Aplica términos y condiciones.</small></p>
           </div>
 
           <div class="col-sm-12 d-flex justify-content-center contCvv">
